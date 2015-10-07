dfa858d55f89a8a4d778deba33764760c6490386
i5ting <i5ting@126.com>
2015-10-07 09:04:20 +0800

    refact

--- post_install.js
+++ post_install.js
@@ -10,14 +10,6 @@ var home_dir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFI
 argv.shift();
 
 var clo_home = home_dir + '/.clo'
-var module_name = argv.shift();
-
-var module_home = clo_home + '/' + module_name;
-// argv.shift();
-console.log('clo_home= ' + clo_home)
-console.log('module_home= ' + module_home)
-console.log('tpl params=' + argv)
-
 
 var mkdirp = require('mkdirp');
     
